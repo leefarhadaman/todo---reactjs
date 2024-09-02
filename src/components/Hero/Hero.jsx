8808864368a287d465b1e7ee34d7884b6dee7317
leefarhadaman <leefarhadaman786@gmail.com>
2024-09-02 23:25:16 +0530

    Initial project setup with structured folders

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Hero.css';
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <h2>Organize Your Tasks Efficiently</h2>
+      <button>Get Started</button>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Navbar } from './Navbar/Navbar';
+export { default as Hero } from './Hero/Hero';
+export { default as Footer } from './Footer/Footer';
+export { default as TodoList } from './TodoList/TodoList';
+export { default as TodoItem } from './TodoItem/TodoItem';
+export { Button } from './common/Button/Button';
+export { Input } from './common/Input/Input';
+    
